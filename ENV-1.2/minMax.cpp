57f0a23ff43e9cb60cf00fc198d1bf727e7d251b
Qiwi303 <rapsdug@gmail.com>
2025-09-14 20:09:06 +0000

    fixed minMax.cpp

--- ENV-1.2/minMax.cpp
+++ ENV-1.2/minMax.cpp
@@ -5,7 +5,13 @@ int main(int argc, char* argv[]){
 	int max = std::stoi(argv[1]);
 
 	for(int i = 2; i < argc; ++i){
-		int x = std::stoi(argv[i]);
+		size_t pos;
+		std::string arr = argv[i];
+		int x = std::stoi(argv[i], &pos);
+		if(pos != arr.size()) {
+			std::cout<<"invalid input"<<std::endl;
+			return 1;
+		}
 		if(x < min) min = x;
 		if(x > max) max = x;
 	}
