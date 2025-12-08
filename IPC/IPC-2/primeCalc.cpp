08b8755a756a40dd86feee88a1dad6d9939642ec
Qiwi303 <rapsdug@gmail.com>
2025-12-08 05:57:40 +0400

    Update primeCalc.cpp

--- IPC/IPC-2/primeCalc.cpp
+++ IPC/IPC-2/primeCalc.cpp
@@ -2,25 +2,22 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-bool checkIfPrime(const int num){
-	if(num < 2){
+bool checkIfPrime(const int num) {
+	if(num < 2) {
 		return false;
 	}
-	
-	if(num == 2){
-		return true;
-	}
-	for(int i = 2; i*i <= num; ++i){
+
+	for(int i = 2; i*i <= num; ++i) {
 		if(num%i == 0) return false;
 	}
 
 	return true;
 }
 
-int findPrime(const int num){
+int findPrime(const int num) {
 	int count = 1;
 	int i = 2;
-	while(count < num){
+	while(count < num) {
 		i++;
 		if(checkIfPrime(i)) count++;
 	}
@@ -28,7 +25,7 @@ int findPrime(const int num){
 	return i;
 }
 
-int main(){
+int main() {
 	int parentToChild[2];
 	int childToParent[2];
 	int num;
@@ -37,61 +34,61 @@ int main(){
 	std::string c = "[Child]";
 
 	int check = pipe(parentToChild);
-	if(check < 0){
+	if(check < 0) {
 		std::cerr<<"Failed to pipe \n";
 		exit(1);
 	}
-		
+
 	check = pipe(childToParent);
-	if(check < 0){
+	if(check < 0) {
 		std::cerr<<"Failed to pipe \n";
 		exit(1);
 	}
-	
+
 	pid_t pid = fork();
 
-	if(pid < 0){
+	if(pid < 0) {
 		std::cerr<<"Failed to fork \n";
 		exit(1);
 	}
 
-	if(pid > 0){
+	if(pid > 0) {
 		check = close(parentToChild[0]);
-		if(check < 0){
+		if(check < 0) {
 			std::cerr<<"Failed to close \n";
-			exit(1); 
+			exit(1);
 		}
 		check = close(childToParent[1]);
-		if(check < 0){
+		if(check < 0) {
 			std::cerr<<"Failed to close \n";
 			exit(1);
 		}
 
-		while(true){
+		while(true) {
 
 			std::cout<<p<<" Please enter the number: ";
 			std::cin>>m;
-			
-				
+
+
 			size_t pos;
-				
-			if(m == "exit"){
+
+			if(m == "exit") {
 				num = -1;
 				check = write(parentToChild[1], &num, sizeof(num));
-				if(check < 0){
+				if(check < 0) {
 					std::cerr<<"Failed to write \n";
 					exit(1);
 				}
 				wait(nullptr);
-				
+
 				check = close(parentToChild[1]);
-				if(check < 0){
+				if(check < 0) {
 					std::cerr<<"Failed to close \n";
 					exit(1);
 				}
-				
+
 				check = close(childToParent[0]);
-				if(check < 0){
+				if(check < 0) {
 					std::cerr<<"Failed to close \n";
 					exit(1);
 				}
@@ -99,13 +96,13 @@ int main(){
 				exit(0);
 			}
 
-			try{	
+			try {
 				num = std::stoi(m, &pos);
-				if(num < 1 || pos != m.size()){
+				if(num < 1 || pos != m.size()) {
 					throw std::invalid_argument("invalid input");
 				}
-			}	
-			catch(...){
+			}
+			catch(...) {
 				std::cerr<<"invalid input \n";
 				continue;
 			}
@@ -113,70 +110,70 @@ int main(){
 			std::cout<<p<<" Sending "<<num<<" to the child process... \n";
 
 			check = write(parentToChild[1], &num, sizeof(num));
-			if(check < 0){
+			if(check < 0) {
 				std::cerr<<"Failed to write \n";
 				exit(1);
 			}
 
-			std::cout<<p<<" Waiting for the response from the child process... \n";	
-			
+			std::cout<<p<<" Waiting for the response from the child process... \n";
+
 			int res;
 			check = read(childToParent[0], &res, sizeof(res));
-			if(check < 0){
+			if(check < 0) {
 				std::cerr<<"Failed to read \n";
 				exit(1);
 			}
 
 			std::cout<<p<<" Received calculation result of prime("<<num<<") = "<<res<<"... \n";
-				
+
 		}
 	}
 
-	if(pid == 0){	
+	if(pid == 0) {
 		check = close(parentToChild[1]);
-		if(check < 0){
+		if(check < 0) {
 			std::cerr<<"Failed to close \n";
 			exit(1);
 		}
 		check = close(childToParent[0]);
-		if(check < 0){
+		if(check < 0) {
 			std::cerr<<"Failed to close \n";
 			exit(1);
 		}
 
-		while(true){
+		while(true) {
 			check = read(parentToChild[0], &num, sizeof(num));
-			if(check < 0){
+			if(check < 0) {
 				std::cerr<<"Failed to read \n";
 				exit(1);
 			}
-		
-			if(num == -1){
+
+			if(num == -1) {
 				check = close(parentToChild[0]);
-				if(check < 0){
+				if(check < 0) {
 					std::cerr<<"Failed to close \n";
 					exit(1);
 				}
 				check = close(childToParent[1]);
-				if(check < 0){
+				if(check < 0) {
 					std::cerr<<"Failed to close \n";
 					exit(1);
 				}
 				exit(0);
 			}
 
-			std::cout<<c<<" Calculating "<<num<<"-th prime number ... \n";	
+			std::cout<<c<<" Calculating "<<num<<"-th prime number ... \n";
 			int res = findPrime(num);
-			
+
 			check = write(childToParent[1], &res, sizeof(res));
 			std::cout<<c<<" Sending calculation result of prime("<<num<<") ... \n";
 
-			if(check < 0){
+			if(check < 0) {
 				std::cerr<<"Failed to write \n";
 				exit(1);
 			}
-		}					
-	}	
-	
+		}
+	}
+
 	return 0;
 }
